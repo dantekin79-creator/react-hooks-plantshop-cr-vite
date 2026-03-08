1b9b53884827ebb7d373145b195a5c5dea2e4db6
dantekin79-creator <dantekin79@gmail.com>
2026-03-08 09:19:21 +0300

    Implement plant shop features: fetch plants, render, add new plant, mark sold out, search, and improved styling

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((r) => r.json())
+      .then(setPlants);
+  }, []);
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const addPlant = (newPlant) => {
+    setPlants([...plants, newPlant]);
+  };
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant} />
+      <Search search={search} setSearch={setSearch} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
